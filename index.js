1d6e3a5c4a863ac33fcc50891cf4ead10bf6cb25
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-07 15:02:06 +0000

    Created a server and saved in a variable

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
+const http = require("http");
+
+/////////////////////////////
+//FILES
 
 //Bocking, synchronous way
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -8,16 +12,21 @@ const fs = require("fs");
 // console.log("File written");
 
 //non-blocking, async way
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
-      console.log(data3);
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log("Your file has been written ✌️");
-      });
-    });
-  });
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("Your file has been written ✌️");
+//       });
+//     });
+//   });
+// });
+// console.log("I was first ha!");
+/////////////////////////
+//SERVER
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server");
 });
-console.log("I was first ha!");
