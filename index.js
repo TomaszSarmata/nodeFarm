1b8e0315a4aa677dddfe6708165cf99705096c4a
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-07 13:56:10 +0000

    Read a file using async c/b

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const fs = require("fs");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+//Bocking, synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `This is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written");
 
-console.log(textIn);
-
-const textOut = `This is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("File written");
+//non-blocking, async way
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  console.log(data);
+});
+console.log("I was first ha!");
