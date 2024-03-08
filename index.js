1628804879e8ed6e2239086666724cc617eee503
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-08 11:34:27 +0000

    Added logic to read the data and convert it into js object from the level of our api route

--- index.js
+++ index.js
@@ -38,6 +38,11 @@ const server = http.createServer((req, res) => {
   } else if (pathName === "/product") {
     res.end("This is the Product");
   } else if (pathName === "/api") {
+    fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
+      const productData = JSON.parse(data);
+      console.log(productData);
+    });
+
     res.end("API");
   } else {
     res.writeHead(404, {
