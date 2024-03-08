93adba01df11575a49fbfe52c227bd3c7622f837
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-08 11:49:29 +0000

    Moved the read statement outside of the req.url so that the file is only read once and stored in a variable data

--- index.js
+++ index.js
@@ -28,6 +28,10 @@ const url = require("url");
 // console.log("I was first ha!");
 /////////////////////////
 //SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObject = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   console.log(req.url);
 
@@ -38,13 +42,10 @@ const server = http.createServer((req, res) => {
   } else if (pathName === "/product") {
     res.end("This is the Product");
   } else if (pathName === "/api") {
-    fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
-      const productData = JSON.parse(data);
-      res.writeHead(200, {
-        "Content-type": "application/json",
-      });
-      res.end(data);
+    res.writeHead(200, {
+      "Content-type": "application/json",
     });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
