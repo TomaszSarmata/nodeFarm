72641715cc98c37059c44fe172206b64a81649f6
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-10 17:15:18 +0000

    Declared three variables to hold data from our templates at the top level of the code so it only gets run once on the initial load

--- index.js
+++ index.js
@@ -29,6 +29,19 @@ const url = require("url");
 /////////////////////////
 //SERVER
 
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObject = JSON.parse(data);
 
