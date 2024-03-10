d9f885c0b5f1d21e816e8f85d68142a20b958694
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-03-10 18:20:29 +0000

    Fixed some typos to test the data in the terminal

--- index.js
+++ index.js
@@ -31,16 +31,16 @@ const url = require("url");
 
 const replaceTemplate = (temp, product) => {
   let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace((/{%IMAGE%}/g, product.image));
-  output = output.replace((/{%PRICE%}/g, product.price));
-  output = output.replace((/{%FROM%}/g, product.from));
-  output = output.replace((/{%NUTRIENTS%}/g, product.nutrients));
-  output = output.replace((/{%QUANTITY%}/g, product.quantity));
-  output = output.replace((/{%DESCRIPTION%}/g, product.description));
-  output = output.replace((/{%ID%}/g, product.id));
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
 
   if (!product.organic)
-    output = output.replace((/{%NOT_ORGANIC%}/g, "not-organic"));
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
   return output;
 };
 
@@ -70,6 +70,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-type": "text/html" });
 
     const cardsHtml = dataObject.map((el) => replaceTemplate(tempCard, el));
+    console.log(cardsHtml);
 
     res.end(tempOverview);
 
